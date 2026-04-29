ad7cf9b69c2c7c2a42a1980e6f64f8d925f6f245
smwasserman <wasserman@mines.edu>
2026-04-29 17:05:22 -0600

    remove home.js and home.json; coded values into index.html

--- js/home.js
+++
@@ -1,7 +0,0 @@
-fetch("./data/home.json")
-  .then((res) => res.json())
-  .then((data) => {
-    document.getElementById("name").textContent = data.name;
-    document.getElementById("title").textContent = data.title;
-    document.getElementById("desc").textContent = data.overview;
-  });
